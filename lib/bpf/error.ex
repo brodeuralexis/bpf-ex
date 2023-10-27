7ef5dc98719b0cbc10bbb0f1c4050c6dac4399bc
Alexis Brodeur <brodeuralexis@gmail.com>
2023-10-27 19:41:22 -0400

    Rework exception modules and add program functions
    We provide a rework of all exceptions in `lib/bpf/exceptions.ex` into a
    new singular exception module `BPF.Error`.  This new module aims to
    construct all errors of the library to ensure consistent error messages.
    
    We also expose a few more functions on the `BPF.Program` module allowing
    configuration of BPF programs.

--- lib/bpf/error.ex
+++ lib/bpf/error.ex
@@ -1,3 +1,46 @@
 defmodule BPF.Error do
-  defexception [:message]
+  defexception [:message, :errno]
+
+  @impl true
+  def exception(opts)
+
+  def exception(op: :object_open, path: path, errno: errno) do
+    message =
+      "failed to open BPF object at #{inspect(path)}, libbpf returned an #{errno} error code."
+
+    %__MODULE__{message: message, errno: errno}
+  end
+
+  def exception(op: :object_open, errno: errno) do
+    message = "failed to open an in-memory BPF object, libbpf returned an #{errno} error code."
+
+    %__MODULE__{message: message, errno: errno}
+  end
+
+  def exception(op: :object_load, errno: errno, log: log) do
+    message =
+      "failed to load BPF object, libbpf returned an #{errno} error code with the following logs: #{log}"
+
+    %__MODULE__{message: message, errno: errno}
+  end
+
+  def exception(op: :object_attach, errno: errno) do
+    message = "failed to attach BPF program, libbpf returned an #{errno} error code."
+
+    %__MODULE__{message: message, errno: errno}
+  end
+
+  def exception(op: :program_pin, path: path, errno: errno) do
+    message =
+      "failed to pin BPF program using path #{inspect(path)}, libbpf returned an #{errno} error code"
+
+    %__MODULE__{message: message, errno: errno}
+  end
+
+  def exception(op: :program_unpin, path: path, errno: errno) do
+    message =
+      "failed to unpin BPF program using path #{inspect(path)}, libbpf returned an #{errno} error code"
+
+    %__MODULE__{message: message, errno: errno}
+  end
 end
--- lib/bpf/program.ex
+++ lib/bpf/program.ex
@@ -18,7 +18,7 @@ defmodule BPF.Program do
         {:ok, %BPF.Link{ref: ref}}
 
       {:error, errno} ->
-        {:error, BPF.AttachError.exception(errno: errno)}
+        {:error, BPF.Error.exception(op: :program_attach, errno: errno)}
     end
   end
 
@@ -31,5 +31,84 @@ defmodule BPF.Program do
       {:error, reason} ->
         raise reason
     end
+  end
+
+  @spec autoload?(t) :: boolean
+  def autoload?(%__MODULE__{} = program) do
+    :bpf_sys.program_autoload(program.ref)
+  end
+
+  @spec set_autoload(t, boolean) :: nil
+  def set_autoload(%__MODULE__{} = program, autoload) when is_boolean(autoload) do
+    _ = :bpf_sys.program_set_autoload(program.ref, autoload)
+  end
+
+  @spec autoattach?(t) :: boolean
+  def autoattach?(%__MODULE__{} = program) do
+    :bpf_sys.program_autoattach(program.ref)
+  end
+
+  @spec set_autoattach(t, boolean) :: nil
+  def set_autoattach(%__MODULE__{} = program, autoattach) when is_boolean(autoattach) do
+    _ = :bpf_sys.program_set_autoattach(program.ref, autoattach)
+  end
+
+  @spec insns(t) :: binary
+  def insns(%__MODULE__{} = program) do
+    :bpf_sys.program_insns(program.ref)
+  end
+
+  @spec set_insns(t, binary) :: nil
+  def set_insns(%__MODULE__{} = program, insns) when is_binary(insns) do
+    _ = :bpf_sys.program_set_insns(program.ref, insns)
+  end
+
+  @spec pin(t, String.t()) :: :ok | {:error, term}
+  def pin(%__MODULE__{} = program, path) when is_binary(path) do
+    case :bpf_sys.program_pin(program.ref, path) do
+      :ok ->
+        :ok
+
+      {:error, errno} ->
+        {:error, BPF.Error.exception(op: :program_pin, path: path, errno: errno)}
+    end
+  end
+
+  @spec pin!(t, String.t()) :: nil
+  def pin!(%__MODULE__{} = program, path) when is_binary(path) do
+    case pin(program, path) do
+      :ok ->
+        nil
+
+      {:error, reason} ->
+        raise reason
+    end
+  end
+
+  @spec unpin(t, String.t()) :: :ok | {:error, term}
+  def unpin(%__MODULE__{} = program, path) when is_binary(path) do
+    case :bpf_sys.program_unpin(program.ref, path) do
+      :ok ->
+        :ok
+
+      {:error, errno} ->
+        {:error, BPF.Error.exception(op: :program_unpin, path: path, errno: errno)}
+    end
+  end
+
+  @spec unpin!(t, String.t()) :: nil
+  def unpin!(%__MODULE__{} = program, path) do
+    case unpin(program, path) do
+      :ok ->
+        nil
+
+      {:error, reason} ->
+        raise reason
+    end
+  end
+
+  @spec unload(t) :: :ok
+  def unload(%__MODULE__{} = program) do
+    :bpf_sys.program_unload(program.ref)
   end
 end
